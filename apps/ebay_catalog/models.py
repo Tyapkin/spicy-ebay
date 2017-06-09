da7f5dd20b6284563074ca71f3d7fed6cd7bce7a
Alexander Tyapkin <atyapkin20@gmail.com>
2017-06-09 15:52:04 +0300

    changes in model

--- apps/ebay_catalog/models.py
+++ apps/ebay_catalog/models.py
@@ -5,14 +5,17 @@ from apps.accounts.models import Credentials
 class Product(models.Model):
     owner = models.ForeignKey(Credentials, on_delete=models.CASCADE)
     upc = models.CharField(max_length=12)
-    image = models.CharField(max_length=255, blank=True)
+    product_id = models.CharField(max_length=12, null=True)
+    image = models.URLField(blank=True)
     rating = models.FloatField(blank=True, null=True)
-    price = models.IntegerField(blank=True, null=True)
-    name = models.CharField(max_length=50, blank=True)
-    in_stock = models.BooleanField(default=True)
-    qty = models.IntegerField(blank=True, null=True)
-    weight = models.FloatField(blank=True, null=True)
-    dimensions = models.FloatField(blank=True, null=True)  # maybe needs all fields e.g. width, height etc.
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    name = models.CharField(max_length=254, blank=True)
+    in_stock = models.BooleanField(default=False)
+    qty = models.IntegerField(blank=True, null=True, default=0)
+    weight = models.FloatField(blank=True, null=True, default=0.0)
+    length = models.FloatField(blank=True, null=True, default=0.0)
+    width = models.FloatField(blank=True, null=True, default=0.0)
+    height = models.FloatField(blank=True, null=True, default=0.0)
     date_updated = models.DateField(auto_now=True)
 
     class Meta:
@@ -21,4 +24,4 @@ class Product(models.Model):
         ordering = ['rating', 'qty', 'date_updated']
 
     def __str__(self):
-        return 'UPC: {} Owner: {}'.format(self.upc, self.owner)
+        return '{}: UPC: {}; Product ID: {}'.format(self.name, self.upc, self.product_id)
